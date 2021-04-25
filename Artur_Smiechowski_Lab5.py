b7b8fe63da5fd981a7dfb1719f4c799615fbe510
Artur Smiechowski <art27833@gmail.com>
2021-04-24 22:14:47 -0400

    Part 2 almost done

---
+++ Artur_Smiechowski_Lab5.py
@@ -0,0 +1,95 @@
+"""
+@author: Artur Smiechowski
+"""
+# %% Imports
+import numpy as np
+from sklearn import preprocessing as skl
+# To import my own module requires this mess
+import sys
+sys.path.insert(1, r'C:\Users\Artur Smiechowski\Documents\BME227_Code\Lab_5_BME_227')
+import Reader
+
+# %% Part 1
+
+# Call the main reading method from the modified part 2 code
+Reader.record_data(com_port='COM3', recording_duration=60, n_channels=3, fs=500, out_string='.')
+
+# %% Part 2
+
+def load_data(name):
+    '''load_data
+    
+    '''
+    emg_data = np.load(str(name) + '_RPS_Data.npy') # Load in data
+    emg_time = np.load(str(name) + '_RPS_Time.npy') # Load in time
+    
+    return emg_data, emg_time # Return loaded data and time
+
+def epoch_data(emg_data, fs, epoch_duration):
+    '''epoch_data
+    
+    '''
+    # Subtract the mean offset from each channel
+    for channel in range(np.size(emg_data, 1)): # Double check the array dimensions
+        mean = np.mean(emg_data[:,channel]) # Find the mean of the current channel
+        emg_data[:,channel] -= mean # Subtract the mean across the current channel
+        #--- Fun fact, -= across an array only works for  numpy arrays, not on lists
+    
+    # epoch_count defined as variable for readability
+    epoch_count = int((np.size(emg_data, 0) / (fs*epoch_duration)) + 0.5) # int and + 0.5 to account for uneven divsion
+    # Initialize epoched_data array
+    #epoched_data = np.zeros(epoch_count, fs*epoch_duration, np.size(emg_data, 1))
+    epoched_data = np.reshape(emg_data, (epoch_count,fs*epoch_duration,np.size(emg_data, 1)))
+    
+    return epoched_data # Return the epoched_data array
+
+def extract_features(epoched_data):
+    '''extract_features
+
+    '''
+    # Initialie the feature sub-matrices
+    epoch_var = np.zeros([np.size(epoched_data, 0), np.size(epoched_data, 2)]) # variance
+    epoch_mav = np.zeros([np.size(epoched_data, 0), np.size(epoched_data, 2)]) # mean absolute value
+    epoch_zc = np.zeros([np.size(epoched_data, 0), np.size(epoched_data, 2)]) # number of zero crossing events
+    
+    for epoch in range(np.size(epoched_data, 0)): # iterate through epochs
+        for channel in range(np.size(epoched_data, 2)): # iterates through channels
+            # Set the values of the variance and MAV sub-matrices
+            epoch_var[epoch, channel] = np.nanvar(epoched_data[epoch, :, channel])
+            epoch_mav[epoch, channel] = np.mean(np.abs(epoched_data[epoch, :, channel]))
+            
+            zc = 0 # zero crossing count
+            prev_sign = np.sign(epoched_data[epoch,0,channel]) # Pulls the sign of the first sample
+            for sample in range(np.size(epoched_data, 1)): # Iterates through all the samples in the current epoch and channel
+                if np.sign(epoched_data[epoch,sample,channel]) != prev_sign: # Check if the sign of the current value is different than previous sign
+                    zc += 1 # Increment zc when sign is different
+                prev_sign = np.sign(epoched_data[epoch,sample,channel])
+            epoch_zc[epoch, channel] = zc # set the epoch and channel zero crossing count to zc
+        
+    # Concatenate the submatrices column-wise into the full feature array
+    features = np.concatenate((epoch_var,epoch_mav,epoch_zc),axis=1)
+    
+    # Normalize the feature array
+    features = skl.scale(features)
+    #--- Cannot get mean and std exactly 0 and 1. Tested on smaller arrays with same syntax and was succesful
+    #--- Output mean and std where X*10^-17 and 0.99999... respectively, assuming floating point error or issue with big array
+    
+    # Create the shorthand list
+    feature_shorthands = ['Var_ch0','Var_ch1','Var_ch2','MAV_ch0','MAV_ch1','MAV_ch2','ZC_ch0','ZC_ch1','ZC_ch2'] # Why is this even in this method?
+    
+    return features, feature_shorthands # Return the normalized feature array
+
+def make_truth_data(action_sequence, epochs_per_action):
+    '''make_truth_data
+    
+    '''
+    instructed_action = [] # Create the empty list for instructed actions
+    for action in range(len(action_sequence)): # I guess we're assuming non-fractional values at this point? (I'd code around that but time)
+        for epoch in range(epochs_per_action): # This could probably be done more elegantly but...
+            instructed_action.append(action_sequence[action]) # Append the current action epochs_per_action times
+        
+    return instructed_action # Returns the array of instructed actions at associated epochs
+
+def crop_mi_inputs(features, truth_labels, included_truth_labels):
+    '''crop_mi_inputs
+    '''
---
+++ Reader.py
@@ -0,0 +1,120 @@
+# -*- coding: utf-8 -*-
+"""
+@author: Artur Smiechowski
+Changed the structure to not use nested methods, if I want to compartmentalize data that's what OOP is for
+"""
+
+#%% import packages
+import numpy as np
+import matplotlib.pyplot as plt
+import serial
+import datetime, pytz
+import os
+
+#%%
+def initialize_plot():
+    '''initialize_plot
+    Initializes the plot for action display
+    
+    Parameters
+    NONE
+    Returns
+    NONE
+    '''
+    # Creates the data range for action vs time
+    display_data_y = np.array([0,1,0,2,0,3]*10)
+    display_data_x = np.arange(0,60)
+    # Creates the step plot
+    plt.step(display_data_x,display_data_y,where='post') # Post fixes aligment
+    
+    actions=['rest','rock','paper','scissors']
+    plt.xlim([-1,60]) # set the x axis limits
+    plt.ylim([0,len(actions)-0.75]) # set the y axis limits 
+    
+    plt.xlabel('time') # add x axis label
+    plt.yticks([0,1,2,3],labels=actions)
+    plt.title('Time Plot of Actions') # add title 
+    #plt.legend(('ch1','ch2','ch3')) # add legend with data from A0 as ch1, A2 as ch2, A3 as ch3
+    
+def initialize_arrays(recording_duration,n_channels,fs):
+        '''
+        this function initializes two arrays. sample_time is a 1d array that holds the 
+        time of each sample recorded. sample_data is a 2D array with n_channels. Each 
+        column is a seperate channels. the channels correspond to sensors on the Arduino
+
+        Parameters
+        ----------
+        recording_duration : int
+            Length of time to run the recording
+        n_channels : int
+            number of sensors hooked up to the Arduino
+        fs : int
+            the sampeling frequency of the arduino 
+
+        Returns
+        -------
+        sample_time : 1D array
+            holds the time of each sample
+        sample_data: 2D array
+            Holds the reading from each sample in each column
+
+        '''
+        sample_time = np.zeros([recording_duration*fs,1])*np.nan # create #sample by 1 array
+        sample_data = np.zeros([recording_duration*fs,n_channels])*np.nan # create #sample by number of channels array
+        
+        return sample_time, sample_data # Was the empty return from before dumping all local variables?
+
+def record_data(com_port='COM4', recording_duration=60, n_channels=3, fs=500, out_string='.'):
+    '''record_data
+    Records emg data from multiple channels for given time
+    
+    Parameters
+    com_port ~ string ~ specifices the port the emg device is on
+    recording_duration ~ int ~ the recording period in seconds
+    n_channels ~ int ~ the number of channels/devices being recorded
+    fs ~ int ~ the emg sampling frequency
+    out_string ~ string ~ the file location to save to
+    
+    Returns
+    NONE
+    '''
+    # Initialize the plot
+    initialize_plot()
+    plt.show() # show the plot
+    print('rest, rock, paper, scissors') 
+    print('Hold each action for 1s')
+   
+    # Initialize the arrays for reading data in
+    sample_time, sample_data= initialize_arrays(recording_duration, n_channels, fs)
+    
+    prompt=input('Press enter to start:') # Just use input to wait for user, why check the input?
+       
+    with serial.Serial(port=com_port, baudrate=500000) as arduino_data: #open connection to serial monitor. takes input com_port form main function
+        
+            
+            n_channel=len(sample_data[0]) #create variable that is the number of columns in sample_data
+            for data_index in range(len(sample_data)): # for loop to index data into arrays
+                try:
+                    arduino_string=arduino_data.readline().decode('ascii') # read line from serial
+                    arduino_list=arduino_string.split() # split string into list at space
+                    sample_time[data_index]=int(arduino_list[0])/1000 # put 1st piece of data into time array
+                       
+                    for channel_index in range(n_channel): # for loop to index data into the corrct column of sample_data and data_line
+                        sample_data[data_index,channel_index]=int(arduino_list[channel_index+1])*5/1024          
+                except: # On dropped data ignore
+                    pass
+                               
+    arduino_data.close() # close searial port
+    '''
+    #Save data 
+    try:
+            os.mkdir(out_string)
+    except OSError:
+            print('save failed')
+    else:
+            print('folder created')
+    '''
+    
+    np.save('Smiechowski_RPS_Data',sample_data) #save sample_data
+    np.save('Smiechowski_RPS_Time',sample_time) #save sample_time
+
